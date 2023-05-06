e084e38169645126809fe071b40b7af6a04540fa
gerald-cakoni <cakonigerald@gmail.com>
2023-05-06 14:18:49 +0200

    New

---
+++ flask/fundamentals/counter/server.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
+
+@app.route('/')
+def index():
+    if 'count' not in session:
+        session['count']=0
+    session['count']+=1
+    if 'countall' not in session:
+        session['countall']=0
+    session['countall']+=1
+    return render_template("index.html", count= session['count'], countall=session['countall'])
+
+  
+
+@app.route('/addtwo')
+def addtwo():
+    session['count']+=1
+    session['countall']+=1
+    return redirect('/')
+
+
+@app.route('/reset')
+def reset():
+    # session.pop('count')
+    session['count']=0
+    return redirect('/')
+
+@app.route('/destroy')
+def destroy():
+    session.clear()
+    return redirect('/')
+
+
+@app.route('/addextra', methods=['POST'])
+def addextra():
+    session['count'] =session['count'] + int(request.form['number']) -1
+    session['countall'] =session['countall'] + int(request.form['number']) -1
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
--- flask/fundamentals/form_test/server.py
+++ flask/fundamentals/form_test/server.py
@@ -1,19 +1,23 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.secret_key = 'keep it secret, keep it safe'
 
 
 @app.route('/')
 def index():
     return render_template("index.html")
 
-
 @app.route('/users', methods=['POST'])
 def create_user():
     print("Got Post Info")
-    print(request.form)
-    # Never render a template on a POST request.
-    # Instead we will redirect to our index route.
-    return redirect('/')
+    # Here we add two properties to session to store the name and email
+    session['username'] = request.form['name']
+    session['useremail'] = request.form['email']
+    return redirect('/show')
+
+@app.route('/show')
+def show_user():
+    return render_template('show.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
